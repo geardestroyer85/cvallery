05dbe67458c70f2a59f2fb150f34fd2f25c5761a
geardestroyer85 <geardestroyer85@gmail.com>
2025-03-03 21:54:21 -0500

    login component

---
+++ packages/client/src/app/features/login/login.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../core/services/auth.service';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [FormsModule, MatFormFieldModule, MatInputModule, MatButtonModule],
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'] // corrected styleUrl to styleUrls
+})
+export class LoginComponent {
+  username: string = '';
+  password: string = '';
+  errorMessage: string | null = null;
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  login() {
+    if (this.username && this.password) {
+      console.log(this.username, this.password)
+      this.authService.login({username: this.username, password: this.password}).subscribe(
+        (response) => {
+          console.log('Login successful:', response);
+          this.router.navigate(['/']); // Redirect after login
+          this.errorMessage = null; // Clear error message on successful login
+        },
+        (error) => {
+          console.error('Login failed:', error);
+          this.errorMessage = error.error.message || 'An error occurred while logging in'; // Set error message
+        }
+      );
+    }
+  }
+}
